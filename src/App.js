f0cfe5e1ab70657afc7c6a4c886574da2dbac4fc
nava6x <navafacts2026@gmail.com>
2024-09-22 16:48:13 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useRef } from "react";
+import { Auth } from "./components/Auth";
+import { Chat } from "./components/Chat";
+import Cookies from "universal-cookie";
+
+import "./App.css"
+const cookies = new Cookies()
+
+function App() {
+  const [isAuth, setIsAuth] = useState(cookies.get("usname"))
+
+  if (!isAuth) {
+  return (
+  <div>
+    <Auth setIsAuth={setIsAuth}/>
+  </div>
+  )
+  }
+
+  return (
+      <Chat room={"chats"}/>
+  )
+}
+
+export default App;
